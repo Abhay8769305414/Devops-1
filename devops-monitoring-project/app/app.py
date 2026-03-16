e20d997a4ed0750161a4651d0c3daee6137b46d5
Abhay8769 <abhaypratapsingh.2022@vitbhopal.ac.in>
2026-03-16 12:23:25 +0530

    Add DevOps monitoring project with CI/CD

---
+++ devops-monitoring-project/app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps Monitoring Demo App"
+
+@app.route("/metrics")
+def metrics():
+    metric_value = random.randint(1,100)
+    
+    data = f"""
+# HELP random_metric Random metric value
+# TYPE random_metric gauge
+random_metric {metric_value}
+"""
+    return Response(data, mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
